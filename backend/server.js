9909a5ea0c69df9b0290e1ce0b58028d781ee445
hanselconsu01 <hanselconsu01@gmail.com>
2025-08-01 18:18:45 -0400

    fix: agregando ruta /date y resolviendo conflicto

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+const itemsRoutes = require('./routes/items');
+const paymentRoutes = require('./routes/payment');
 const dateRoutes = require('./routes/date');
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.use('/items', itemsRoutes);
+app.use('/payment', paymentRoutes);
 app.use('/date', dateRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
